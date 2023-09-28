7cd5d8fec02788d6e897cb7abc8db5569ea7f0f6
tubayzc <nur97tuba@gmail.com>
2023-09-28 15:16:27 +0300

    Event-detail ve adminPage de düzenleme yapıldı.

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.component.html',
-  styleUrls: ['./event-detail.component.css']
+  styleUrls: ['./event-detail.component.css'],
 })
-export class EventDetailComponent {
+export class EventDetailComponent {}
 
-}
+// TypeScript ile seçilen tabı göster veya gizle
+const tabs: NodeListOf<Element> = document.querySelectorAll('.tab-button');
+const tabContents: NodeListOf<Element> =
+  document.querySelectorAll('.tab-content');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    // Tüm tabları gizle
+    tabContents.forEach((content) => {
+      content.classList.add('hidden');
+    });
+
+    // Tıklanan tabın içeriğini göster
+    const targetId: string | null = tab.getAttribute('data-tabs-target');
+    const targetContent: Element | null = document.querySelector(
+      targetId || ''
+    );
+    if (targetContent) {
+      targetContent.classList.remove('hidden');
+    }
+  });
+});
