794ade8b4f6df70d08d85ed19b76288f3a9efabf
tubayzc <nur97tuba@gmail.com>
2023-10-12 13:22:32 +0300

    EventDetail bitti.

--- src/core/models/commentLike.model.ts
+++ src/core/models/commentLike.model.ts
@@ -1,23 +1,10 @@
+import { Comment } from './comment.model';
+import { User } from './user.model';
 
-import { Comment } from "./comment.model";
-import { User } from "./user.model";
-
-export class CommentLike{
-    id?: number;
-    commentId?: number;
-    userId?: number;
-    user?:User;
-    comment?:Comment;
-    
+export class CommentLike {
+  id?: number;
+  commentId?: number;
+  userId?: number;
+  user?: User;
+  comment?: Comment;
 }
-import { User } from "./user.model";
-import { Comment } from "./comment.model";
-
-        export class CommentLike{
-            id?:number;
-            userId?:number;
-            commentId?:number;
-            user?:User;
-            comment?:Comment
-        }
-
