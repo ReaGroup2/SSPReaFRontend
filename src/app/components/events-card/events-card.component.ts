a98e1d0dc43a499f00a7808fd984c49221cb9588
İlker Şenel <ilkersenel5797@gmail.com>
2023-10-18 22:00:21 +0300

    etkinlik sayfası düzenlemesi yapıldı

--- src/app/components/events-card/events-card.component.ts
+++ src/app/components/events-card/events-card.component.ts
@@ -9,63 +9,67 @@ import { Router } from '@angular/router';
   styleUrls: ['./events-card.component.css']
 })
 export class EventsCardComponent {
-@Input() cardColor: String ="light blue";
-cardColors=["light blue","light green","light yellow","light red"];
+  @Input() cardColor: String = "light blue";
+  cardColors = ["light blue", "light green", "light yellow", "light red"];
+  pageCount: number = 0;
 
-constructor(private service:ApiService,private router:Router){}
+  constructor(private service: ApiService, private router: Router) { }
 
-events?: Event[];
-currentPage: number = 1;
-  
+  events?: Event[];
+  currentPage: number = 1;
+  pageCardCount:number=2;
+
   ngOnInit(): void {
     this.getAllEvents();
     this.getDisplayedEvents();
-    
 
-  
+
+
   }
-  
-  
+
+
   getAllEvents() {
     this.service.getAllEntities(Event).subscribe((response) => {
-      this.events = response.data;      
-      
-  } );
-}
+      this.events = response.data;
 
-eventDetail(id1:any){
-  this.router.navigate(['/event-detail'], { queryParams: { id: id1 } });
-
-}
-
-getDisplayedEvents(): Event[] {
-  const startIndex = (this.currentPage - 1) * 1;
-  const endIndex = Math.min(startIndex + 1, this.events!.length);
-  console.log(this.events!.slice(startIndex, endIndex));
-  return this.events!.slice(startIndex, endIndex);
-}
-nextPage() {
-  if (this.currentPage < Math.ceil(this.events!.length / 1)) {
-    this.currentPage++;
+    });
   }
-  this.getDisplayedEvents();
-}
 
-prevPage() {
-  if (this.currentPage > 1) {
-    this.currentPage--;
+  eventDetail(id1: any) {
+    this.router.navigate(['/event-detail'], { queryParams: { id: id1 } });
+
   }
-  this.getDisplayedEvents();
-}
 
-generateNumbers(count: number): number[] {
-  return Array.from({ length: count }, (_, index) => index + 1);
-}
+  getDisplayedEvents(): Event[] {
+    const startIndex = (this.currentPage - 1) * this.pageCardCount;
+    const endIndex = Math.min(startIndex + this.pageCardCount, this.events!.length);
+    console.log(this.events!.slice(startIndex, endIndex));
+    this.pageCount = Math.round(this.events!.length / this.pageCardCount)
+    return this.events!.slice(startIndex, endIndex);
 
-goThatPage(num:number){
-  this.currentPage=num;
-  this.getDisplayedEvents();
-}
+  }
+  nextPage() {
+    if (this.currentPage < Math.ceil(this.events!.length / this.pageCardCount)) {
+      this.currentPage++;
+    }
+    this.getDisplayedEvents();
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+    this.getDisplayedEvents();
+  }
+
+  generateNumbers(count: number): number[] {
+    return Array.from({ length: count }, (_, index) => index + 1);
+  }
+
+  goThatPage(num: number) {
+    this.currentPage = num;
+    this.getDisplayedEvents();
+  }
 
 
 
